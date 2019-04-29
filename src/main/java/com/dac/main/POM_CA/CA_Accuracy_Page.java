c8e4bf9be24d837fab0bb80961bce5673fc860f6
prathibha32 <prathibhasnair32@gmail.com>
2019-04-29 21:26:50 +0530

    Added new method. Added New class!

---
+++ src/main/java/com/dac/main/POM_CA/CA_Accuracy_Page.java
@@ -0,0 +1,189 @@
+package com.dac.main.POM_CA;
+
+import static org.testng.Assert.assertTrue;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import resources.CurrentState;
+import resources.ExcelHandler;
+
+public class CA_Accuracy_Page extends CA_abstractMethods{
+	
+	WebDriver driver;
+	Actions action;
+	WebDriverWait wait;
+
+	public CA_Accuracy_Page(WebDriver driver) {
+
+		super(driver);
+		this.driver = driver;
+		wait = new WebDriverWait(driver, 35);
+		action = new Actions(driver);
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(id="myGroups")
+	private WebElement filterGroup;
+	
+	@FindBy(xpath="//*[@class='menu transition hidden']")
+	private WebElement filterDropDown;
+	
+	@FindBy(xpath="(//*[@id='divCIAccuracy']/h1)")
+	private WebElement CATitleContent;
+	
+	// Global Filter locators
+
+		@FindBy(css = "div.ui.fluid.search.selection.dropdown.myList")
+		private WebElement FilterCountry;
+
+		@FindBy(css = "div.ui.fluid.search.selection.dropdown.myList1")
+		private WebElement FilterState;
+
+		@FindBy(css = "div.ui.fluid.search.selection.dropdown.myList2")
+		private WebElement FilterCity;
+
+		@FindBy(css = "div.ui.fluid.search.selection.dropdown.myList3")
+		private WebElement Filterlocation;
+
+		@FindBy(css = "button#apply_filter")
+		private WebElement Apply_filter;
+		
+	// export button
+	@FindBy(css = "button#btnExport>span")
+	private WebElement exportBtn;
+	
+	// overview report
+	@FindBy(css = "div#compIntOverviewContainer")
+	private WebElement overviewReport;
+	
+	// section of overview report
+	@FindBy(xpath = "//div[@id='compIntOverviewContainer']//div[starts-with(@class,'overviewSubContainer')]")
+	private List<WebElement> competitors;
+
+	String xpathCompetitors = "(//div[@id='compIntOverviewContainer']//div[starts-with(@class,'overviewSubContainer')])";
+	String compName = ".//div[starts-with(@class, 'competitorName')]";
+	String compScore = ".//div[starts-with(@class, 'competitorScore')]";
+	
+	// site table
+	@FindBy(css = "table#compIntAccuracySitesTable")
+	private WebElement siteTable;
+
+	List<WebElement> columns;
+	List<WebElement> rows;
+
+
+	public void exportaccuracyReport() throws InterruptedException, FileNotFoundException, IOException {
+		waitForElement(overviewReport, 10);
+		waitForElement(exportBtn, 10);
+		scrollByElement(exportBtn);
+		download(CurrentState.getBrowser(), exportBtn, 20);
+		convertExports(getLastModifiedFile(Exportpath), AccuracyExport);
+	}
+
+
+	
+	@Override
+	public List<Map<String, String>> getOverviewReport() {
+		// TODO Auto-generated method stub
+		waitForElement(overviewReport, 10);
+		scrollByElement(overviewReport);
+		Map<String, String> kMap;
+		List<Map<String, String>> ovrwRprtData = new ArrayList<Map<String, String>>();
+		for (int i = 1; i <= competitors.size(); i++) {
+			WebElement s = driver.findElement(By.xpath(xpathCompetitors + "[" + i + "]"));
+			// ovrwRprtData.put("Score", s.findElement(By.xpath(compScore)).getText());
+			kMap = new HashMap<String, String>();
+			kMap.put("compName", s.findElement(By.xpath(compName)).getText());
+			kMap.put("score", s.findElement(By.xpath(compScore)).getText());
+			System.out.format("%10s%10s", s.findElement(By.xpath(compName)).getText(),
+					s.findElement(By.xpath(compScore)).getText());
+			System.out.println("");
+			ovrwRprtData.add(kMap);
+		}
+		return ovrwRprtData;
+	}
+//	@Override
+//	public List<Map<String, String>> getOverviewReport() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+	
+	public void verify_pageloadCompletely(int timeout) {
+		if (waitForElement(overviewReport, timeout) && waitForElement(siteTable, timeout)
+				& waitForElement(filter_Panel, timeout))
+			assertTrue(true, "All sections filter, overview report, site table and graph is loaded");
+		else
+			assertTrue(false, "Page not loaded completely");
+	}
+	
+	public List<Map<String, String>> getExportData() throws Exception {
+		exportaccuracyReport();
+
+		String[][] table = new ExcelHandler(Exportpath + AccuracyExport, "Sheet0").getExcelTable();
+		List<Map<String, String>> exportData = new ArrayList<Map<String, String>>();
+		int colSize = table[0].length;
+		for (int col = 1; col < colSize; col++) {
+			Map<String, String> kMap = new HashMap<String, String>();
+
+			for (int i = 1; i < table.length; i++) {
+				kMap.put("compName", table[0][col]);
+				kMap.put(table[i][0], table[i][col]);
+			}
+			exportData.add(kMap);//to be reviewed
+		}
+		
+
+		return exportData;
+
+	}
+	
+	public void compareExportnTable(List<Map<String, String>> exportData, List<Map<String, String>> siteTableData) {
+		
+		for (Map<String, String> m1 : exportData) {
+			for (Map<String, String> m2 : siteTableData) {
+				
+				if (m1.get("compName").equals(m2.get("compName"))) {
+					
+					Assert.assertEquals(m1.size() - 1, m2.size());
+					m1.forEach((k,v)->{
+						System.out.println("for name " + k + " score from export " + v
+						+ "and score from site table" + m2.get(k));
+						if (!k.equalsIgnoreCase("Overall") && !k.contains("Yellowpages")
+								&& !k.equals("compName")) {
+							Assert.assertEquals(formatFloat(v), formatFloat(m2.get(k)), 0.05,
+									"Verifying score for " + k + "for" + m1.get("compName"));
+						} else if (k.equalsIgnoreCase("YellowpagesCom")) {
+							Assert.assertEquals(formatFloat(v), formatFloat(m2.get("Yellowpages.com")), 0.05,
+									"Verifying score for " + k + "for" + m1.get("compName"));
+						} else if (k.equalsIgnoreCase("Yellowpages")) {
+							Assert.assertEquals(formatFloat(v), formatFloat(m2.get("Yellowpages.ca")), 0.05,
+									"Verifying score for " + k + "for" + m1.get("compName"));
+	
+						}
+					
+				});
+					
+				}
+			}
+
+		}
+
+	}
+
+	
+
+}
--- src/test/java/com/dac/testcases/CA/CA_Visibility_Test.java
+++ src/test/java/com/dac/testcases/CA/CA_Visibility_Test.java
@@ -1,19 +1,25 @@
 package com.dac.testcases.CA;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.Status;
 import com.dac.main.Navigationpage;
 import com.dac.main.POM_CA.CA_Visibility_Page;
+import com.dac.main.POM_CA.CA_gatherData;
+import com.dac.main.POM_SA.SA_ReviewReportCard_Page;
+import com.dac.main.POM_SA.SA_gatherData;
 import com.selenium.testevidence.SeleniumEvidence;
 
 import junit.framework.Assert;
 import resources.BaseClass;
 import resources.CurrentState;
+import resources.ExcelHandler;
 
 public class CA_Visibility_Test extends BaseClass {
 
@@ -23,11 +29,12 @@ public class CA_Visibility_Test extends BaseClass {
 	CA_Visibility_Page data;
 
 	@SuppressWarnings("unchecked")
-  @Test(groups= {"smoke"},description = "Test for navigating to Visibility page")
+//  @Test(groups= {"smoke"},description = "Test for navigating to Visibility page")
+//	  @Test(groups= {"smoke"},description = "Test for navigating to Visibility page with all filter condition check")
+	@Test(enabled = true)
 	public void navigateToVisibilityPage() throws Exception {
 		np = new Navigationpage(CurrentState.getDriver());
 	np.navigateCA_Visibility();
-		 /*new CA_Visibility_Page(CurrentState.getDriver()).verify_pageloadCompletely(10);*/
 		 CurrentState.getLogger().log(Status.PASS,
 		          "Navigated successfully to CA Visibility page");    
 		      addEvidence(CurrentState.getDriver(), "Navigate to Visibility page from Dashboard", "yes"); 
@@ -36,15 +43,39 @@ public class CA_Visibility_Test extends BaseClass {
 	}
 
 	@SuppressWarnings("unchecked")
-  @Parameters({ "Filter" })
+//  @Parameters({ "Filter" })
+	//Filter Condition Apply
 	@Test(dependsOnMethods = { "navigateToVisibilityPage" }, groups= {"smoke"}, description = "Verify Visibility page loads after filter applied")
-	public void verifyFilteringReportsVisibility(String Filter) throws Exception {
-		data = new CA_Visibility_Page(CurrentState.getDriver());
-		String[] filter = Filter.split(",");
-		data.applyFilter(filter[0], filter[1], filter[2], filter[3]);
-		data.verify_pageloadCompletely(10); 
-    addEvidence(CurrentState.getDriver(), "Apply Filter from Visibility page", "yes"); 
-	}
+	public void verifyFilteringReportsVisibility() throws Exception {try {	
+		int count = 1;
+		ExcelHandler wb = new ExcelHandler("./data/FilterCriteria.xlsx", "CA_FIL"); wb.deleteEmptyRows();
+		CA_Visibility_Page s = new CA_Visibility_Page(CurrentState.getDriver());
+		for(int i=1;i<=wb.getRowCount();i++) {
+			System.out.println("*******************  Scenarios : "+ count +"Starts ****************************");
+			if(i>1) CurrentState.getDriver().navigate().refresh();
+			s.waitUntilLoad(CurrentState.getDriver());
+			
+			
+			String CountryCode = wb.getCellValue(i, wb.seacrh_pattern("Country", 0).get(0).intValue());
+			String State = wb.getCellValue(i, wb.seacrh_pattern("State", 0).get(0).intValue());
+			String City = wb.getCellValue(i, wb.seacrh_pattern("City", 0).get(0).intValue());
+			String Location = wb.getCellValue(i, wb.seacrh_pattern("Location", 0).get(0).intValue());
+			
+			s.applyFilter(CountryCode, State, City, Location);
+			System.out.println(CountryCode+", "+State+", "+City+", "+Location);
+			s.clickApplyFilterBTN();
+			BaseClass.addEvidence(CurrentState.getDriver(),
+					"Applied global filter: "+CountryCode+", "+State+", "+City+", "+Location+"", "yes");
+			
+			Thread.sleep(4000);
+			System.out.println("-------------- Scenarios : "+ count++ + "Ends --------------------");
+			
+		}
+	}catch(Exception e) {
+		e.printStackTrace();
+		//Assert.fail("");
+	}}
+	
 
 	@SuppressWarnings("unchecked")
   @Test(dependsOnMethods = { "navigateToVisibilityPage"}, groups= {"smoke"}, description = "Test for overview export and export verification")
