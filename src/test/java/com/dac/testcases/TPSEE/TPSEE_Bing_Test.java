d028082065f770679ca1db02ee723504cd78c67a
avinash <avinash@LapWinX-DAC13.arackal.co.in>
2020-01-06 15:57:42 +0530

    1)Added manual date selection code					2)Added manual date selection code

--- src/test/java/com/dac/testcases/TPSEE/TPSEE_Bing_Test.java
+++ src/test/java/com/dac/testcases/TPSEE/TPSEE_Bing_Test.java
@@ -1,13 +1,18 @@
 package com.dac.testcases.TPSEE;
 
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.Status;
 import com.dac.main.Navigationpage;
 import com.dac.main.POM_TPSEE.TPSEE_Bing_Page;
+import com.dac.main.POM_TPSEE.TPSEE_GMB;
 
 import resources.BaseClass;
 import resources.CurrentState;
@@ -19,6 +24,11 @@ public class TPSEE_Bing_Test extends BaseClass{
 	Navigationpage np;
 	TPSEE_Bing_Page data;
 	String grph = "div.highcharts-label.highcharts-tooltip-box.highcharts-color-none";
+	int start = 980;
+	int end = 0;
+	String grphfromDate = "(//*[@class='highcharts-label highcharts-range-input'])[1]";
+	String grphtoDate = "(//*[@class='highcharts-label highcharts-range-input'])[2]";
+	
 	
 	//Test to navigate to Bing Page
 	@Test(priority=1,groups = { "smoke" }, description = "Test for navigating to Bing page")
@@ -105,8 +115,6 @@ public class TPSEE_Bing_Test extends BaseClass{
 			public void gethighchartsdate() throws Exception{
 				data = new TPSEE_Bing_Page(CurrentState.getDriver());
 				String OneMonth ="1m";
-				int start = 980;
-				int end = 0;
 				data.clickHighchartCriteria(OneMonth,start,end,grph);
 				addEvidence(CurrentState.getDriver(), "one Month Zoom functionality", "yes");
 				Thread.sleep(5000);
@@ -130,4 +138,62 @@ public class TPSEE_Bing_Test extends BaseClass{
 				data.clickHighchartCriteria(ALLDATA,start,end,grph);
 				addEvidence(CurrentState.getDriver(), "All Data Zoom functionality", "yes");
 				}
+			
+			@Test(priority = 8,enabled = true, dataProvider = "testData")
+			public void SetCalendarDate(String from_day, String from_month, String from_year, String to_day, String to_month, String to_year) throws Exception {
+				
+				data = new TPSEE_Bing_Page(CurrentState.getDriver());
+				if(!(from_day.equals("null")) | !(to_day.equals("null")) ) {
+					data.selectCalender_FromDate(grphfromDate,(int)(Double.parseDouble(from_day)), from_month, (int)(Double.parseDouble(from_year)));
+					addEvidence(CurrentState.getDriver(), "SetCalendarDate", "Yes");
+					data.selectCalender_ToDate(grphtoDate,(int)(Double.parseDouble(to_day)), to_month, (int)(Double.parseDouble(to_year)));	
+					addEvidence(CurrentState.getDriver(), "SetCalendarDate", "Yes");				
+					Date fromcal = data.getCurrentfromDate();
+					Date fromgrph = data.verifyinitialHistoryGraph(start, end, grph);
+					addEvidence(CurrentState.getDriver(), "SetCalendarDate", "Yes");
+					Assert.assertEquals(fromgrph, fromcal);
+					Date tocal = data.getCurrenttoDate();
+					Date togrph = data.verifyfinalHistorygraph(grph);
+					Assert.assertEquals(togrph, tocal);
+					addEvidence(CurrentState.getDriver(), "SetCalendarDate", "Yes");
+				}				
+			}
+			
+			@DataProvider
+			public String[][] testData(){
+				String[][] data = null, data1 = null;
+				try {
+				ExcelHandler wb = new ExcelHandler("./data/Filter.xlsx", "Zoom");
+				wb.deleteEmptyRows();
+				int rowCount = wb.getRowCount();
+				System.out.println("rowCount : "+rowCount);
+				data = new String[rowCount-1][6];
+				data1 = new String[rowCount-1][6];
+				int row = 0;
+				for(int i = 2; i<=rowCount;i++) {
+					 int colCount = wb.getColCount(i);
+					 for(int j = 0;j<colCount; j++) {
+						 if(j > 0) {
+							 if(((wb.getCellValue(i, j).trim()).equalsIgnoreCase("null")) | 
+									 ((wb.getCellValue(i, j).trim()).length() == 0)){
+								 data1[row][j] = "null";
+							 }else data1[row][j] = wb.getCellValue(i, j).trim();
+						 }else data1[row][j] = wb.getCellValue(i, j).trim();		 
+					 }
+					 row++;
+					 }
+					 data[0][0] = wb.getCellValue(2, 0);  // from day
+					 data[0][1] = wb.getCellValue(2, 1);  // from month
+					 data[0][2] = wb.getCellValue(2, 2);  // from year
+					 data[0][3] = wb.getCellValue(2, 3);  // to day
+					 data[0][4] = wb.getCellValue(2, 4);  // to month
+					 data[0][5] = wb.getCellValue(2, 5);  // to year
+					 System.out.println("Arrays.deepToString(data) : "+Arrays.deepToString(data));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				finally {
+					return data;
+				}
+			}
 }
