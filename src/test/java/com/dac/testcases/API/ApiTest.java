565278dd6ce7d25e2699caf668a509a2c9e60a24
aneeshc <aneeshc@LapWinX-DAC57.arackal.co.in>
2020-03-12 19:15:41 +0530

    api changes

--- src/test/java/com/dac/testcases/API/ApiTest.java
+++ src/test/java/com/dac/testcases/API/ApiTest.java
@@ -1,21 +1,26 @@
 package com.dac.testcases.API;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import com.dac.main.api.ApiBase;
+
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
-public class ApiTest {
+public class ApiTest extends ApiBase{
 	
 	@Test
 	public void setRequest() {
-		RestAssured.baseURI = "http://ldmbluebeta.azurewebsites.net/api";
-		RequestSpecification httpRequest = RestAssured.given();
-		Response response = httpRequest.request(Method.GET,"/LocationHistoryGetByLocationID/5e950b02-32ed-47e3-b5f4-77ac8e02af90");
-		int StatusCode = response.getStatusCode();
-		System.out.println("Response"+response.asString());
-	Assert.assertEquals(StatusCode, 200);
+//		//RestAssured.baseURI = "http://ldmbluebeta.azurewebsites.net/api";
+//		RequestSpecification httpRequest = RestAssured.given();
+//		Response response = httpRequest.request(Method.GET,"http://ldmbluebeta.azurewebsites.net/api/LocationHistoryGetByLocationID/5e950b02-32ed-47e3-b5f4-77ac8e02af90");
+//		int StatusCode = response.getStatusCode();
+//		System.out.println("Response"+response.asString());
+//		Assert.assertEquals(StatusCode, 200);
+		
+		getRequest("http://ldmbluebeta.azurewebsites.net/api/LocationHistoryGetByLocationID/5e950b02-32ed-47e3-b5f4-77ac8e02af90", "200", "");
 	}
 	
 	
