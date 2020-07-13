c249900dca939384a0ea81577ecdb07f5bcbc002
aneeshc <aneeshc@LapWinX-DAC57.arackal.co.in>
2020-07-13 22:31:44 +0530

    statefarm test

--- src/test/java/test/Statefarm.java
+++ src/test/java/test/Statefarm.java
@@ -27,9 +27,10 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Statefarm {
 
 	ExcelHandler excel = new ExcelHandler();
-	//String filePath ="C:\\Users\\aneeshc\\Desktop\\Statefarm - Copy.xlsx";
-	String filePath ="C:\\Users\\aneeshc\\Desktop\\Statefarm.xlsx";
-	String outputFilePath = "C:\\Users\\aneeshc\\Desktop\\RCF Lines - Copy.xlsx";
+	String filePath ="C:\\Users\\aneeshc\\Desktop\\Statefarm - Copy.xlsx";
+	String sheetname = "Sheet4";
+//	String filePath ="C:\\Users\\aneeshc\\Desktop\\Statefarm.xlsx";
+//	String outputFilePath = "C:\\Users\\aneeshc\\Desktop\\RCF Lines - Copy.xlsx";
 	//String outputFilePath = "C:\\Users\\aneeshc\\Desktop\\t.xlsx";
 	String phnum;
 	String expected;
@@ -43,11 +44,11 @@ public class Statefarm {
 	public void beforeclass() throws IOException {
 		WebDriverManager.chromedriver().version("83.0.4103.39").setup(); 
 		driver = new ChromeDriver();
-		count=2702;
+		count=1;
 
-		fis = new FileInputStream(outputFilePath);
+		fis = new FileInputStream(filePath);
 		workbook = new XSSFWorkbook(fis);
-		sheet = workbook.getSheet("Sheet1");
+		sheet = workbook.getSheet(sheetname);
 
 		//	ExcelHandler exc = new ExcelHandler("outputFilePath","Sheet1");
 	}
@@ -55,7 +56,7 @@ public class Statefarm {
 	@AfterClass
 	public void afterclass() throws IOException {
 		driver.quit();
-		FileOutputStream fos = new FileOutputStream(outputFilePath);
+		FileOutputStream fos = new FileOutputStream(filePath);
 		workbook.write(fos);
 		workbook.close();
 		fos.close();
@@ -65,34 +66,34 @@ public class Statefarm {
 	@DataProvider
 	public Object[][] test() throws Exception
 	{
-		return excel.getData(filePath, "Sheet1", "ST");
+		return excel.getData(filePath, sheetname, "ST");
 	}	
 
 	@Test(priority = 1,dataProvider = "test")
 	public void tc1(HashMap<String, String> h) throws IOException {
 		count++;
-		expected = h.get("PhNum");
+		expected = h.get("RCF LINE");
 
 		try {
-			url = h.get("URL");
+			url = h.get("LP URL");
 
 			driver.get(url);
 			Thread.sleep(1000);
 			phnum = driver.findElement(By.id("logo-bar-phone")).getText();
 
 			if(phnum.equals(expected)) {
-				System.out.println(+count+" || "+url+" // UI value "+phnum+" excel value "+h.get("PhNum"));
+				System.out.println(+count+" || "+url+" // UI value "+phnum+" excel value "+expected);
 			}
 			else {				
 				Thread.sleep(2000);
 				phnum = driver.findElement(By.id("logo-bar-phone")).getText();
 				if(phnum.equals(expected))
-					System.out.println(+count+" || "+url+" // UI value "+phnum+" excel value "+h.get("PhNum"));
+					System.out.println(+count+" || "+url+" // UI value "+phnum+" excel value "+expected);
 				else
 				{
-					System.err.println(+count+" || "+url+" //Failed  UI value "+phnum+" excel value "+h.get("PhNum"));	
-				sheet.getRow(count-1).createCell(5).setCellValue("Failed");
-				sheet.getRow(count-1).createCell(6).setCellValue("Incorrect Phone Number");
+					System.err.println(+count+" || "+url+" //Failed  UI value "+phnum+" excel value "+expected);	
+				sheet.getRow(count-1).createCell(4).setCellValue("Failed");
+				sheet.getRow(count-1).createCell(5).setCellValue("Incorrect Phone Number");
 				//assertEquals(phnum, expected);
 				}
 			}
@@ -100,9 +101,10 @@ public class Statefarm {
 
 
 		catch(Exception e){
+			//e.printStackTrace();
 			System.err.println(+count+" || "+url+" // Page not found ");	
-			sheet.getRow(count-1).createCell(5).setCellValue("Failed");
-			sheet.getRow(count-1).createCell(6).setCellValue("Page not found");
+			sheet.getRow(count-1).createCell(4).setCellValue("Failed");
+			sheet.getRow(count-1).createCell(5).setCellValue("Page not found");
 
 		}
 	}
